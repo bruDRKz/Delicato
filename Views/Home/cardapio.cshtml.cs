14fdefaf433920db6989d39fb501f2b3dfe93fc5
guszzz111 <guszv@hotmail.com>
2025-11-11 15:10:44 -0300

    cardapio e imagens arrumadas
    imagens que estavam com cdn foram trocadas para as do assets
    Cardapio com css junto tbm

---
+++ Views/Home/cardapio.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DelicatoProject.Views.Home
+{
+    public class cardapioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
